840bff313e76d6e69575daaac4a1735995ced8c9
Daniel Cortes Pichardo <daniel.cortes.pichardo@gmail.com>
2017-12-13 22:33:57 -0600

    fist version of kukulkan-shell

---
+++ src/main/java/mx/infotec/dads/kukulkan/shell/kukulkanshell/KukulkanShellApplication.java
@@ -0,0 +1,12 @@
+package mx.infotec.dads.kukulkan.shell.kukulkanshell;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KukulkanShellApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KukulkanShellApplication.class, args);
+	}
+}
